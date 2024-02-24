30b5133d3f4f70b678c4de5d0e8eba76c2019346
mohmmedabdelaal <medo.elaal@gmail.com>
2024-02-24 02:43:41 +0200

    hangman game with python

---
+++ hangman/main.py
@@ -0,0 +1,46 @@
+# Step 5
+
+import random
+import hangman_art
+import wordlist
+
+chosen_word = random.choice(wordlist.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+# Testing code
+print(f'{hangman_art.logo}.')
+
+# Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    # TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    print(hangman_art.stages[lives])
